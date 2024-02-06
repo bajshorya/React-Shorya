ef1e7d6c0d14a72a6dcc0d4c1de4500cbdc4e16a
bajshorya <jain.shorya11@gmail.com>
2024-02-07 00:54:29 +0530

    made counter project via git

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import './App.css'
+function App() {
+//hooks
+  let[counter,setCounter]=useState(0)
+//counter - the var that will be changed
+//setCounter-asks for the new value that will be assigned to counter
+  // let counter=2;
+  const addValue=()=>{
+    if(counter<20){
+      // counter+=1;
+      setCounter(counter+1)
+    }return;
+    
+    // setCounter(counter) or directly pass it here after incrementing it above
+  }
+  const removeValue=()=>{
+    if(counter>0){
+      setCounter(counter-1)
+    }return;
+    // counter+=1;
+    
+  }
+  return (
+    <>
+    <h1>Shorya</h1>
+    <h2>counter value {counter}</h2>
+
+    <button
+    onClick={addValue}>Add value</button>
+    <br />
+    <button
+    onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+export default App
+//UI updation is controlled by react 
+// hooks 
+
+//change propogation
