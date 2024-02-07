8cc78eb0ca5eb571baa23de83fe44b7125b412a0
bajshorya <jain.shorya11@gmail.com>
2024-02-07 18:15:13 +0530

    prev counter question

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -10,6 +10,20 @@ function App() {
     if(counter<20){
       // counter+=1;
       setCounter(counter+1)
+      // setCounter(counter+1)
+      // setCounter(counter+1)
+      // setCounter(counter+1)
+      // setCounter(counter+1)
+
+      //even if we made this number of calls this wont increase the count immediatly by 5 , beacause of useState
+      // as it will combine them and send them in batches 
+      // to do this make a prevcounter 
+
+      // setCounter(prevCounter=>prevCounter+1)
+      // setCounter(prevCounter=>prevCounter+1)
+      // setCounter(prevCounter=>prevCounter+1)
+
+
     }return;
     
     // setCounter(counter) or directly pass it here after incrementing it above
