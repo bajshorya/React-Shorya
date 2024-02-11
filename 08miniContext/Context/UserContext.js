b0c1898e7f6d8cd3fcdab4028e64c9ff14351106
bajshorya <jain.shorya11@gmail.com>
2024-02-11 15:11:18 +0530

    context !usage define

--- 08miniContext/Context/UserContext.js
+++ 08miniContext/Context/UserContext.js
@@ -2,3 +2,7 @@ import React from "react";
 const UserContext =React.createContext()
 
 export default UserContext;
+
+
+
+//context makes it easier to use component in other files by just using context and not using states of that component again & again ... This makes working of complex websites much easier !!!!!!!!!!
