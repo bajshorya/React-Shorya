31730442a8eb8704fadab95f3e937d173d636ba6
bajshorya <jain.shorya11@gmail.com>
2024-02-17 21:26:22 +0530

    RTE PostForm

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -4,5 +4,6 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn, RTE };
