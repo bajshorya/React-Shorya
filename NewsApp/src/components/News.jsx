aa8b966bd8fa21f05474d67acbc29328734c71a3
SHORYA BAJ <jain.shorya11@gmail.com>
2024-02-24 14:52:31 +0530

    fetched api from news api

--- NewsApp/src/components/News.jsx
+++ NewsApp/src/components/News.jsx
@@ -4,9 +4,19 @@ import NewsItem from "./NewsItem";
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news Component
-        <NewsItem />
+      <div className="container my-3">
+        <h2>NewsMonkey - Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="my title" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="my title" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="my title" description="mydesc" />
+          </div>
+        </div>
       </div>
     );
   }
--- NewsApp/src/components/NewsItem.jsx
+++ NewsApp/src/components/NewsItem.jsx
@@ -2,6 +2,22 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    return <div>This is a News Item</div>;
+    let { title, description } = this.props;
+    return (
+      <div className="card" style={{ width: "18rem" }}>
+        <img
+          src="https://sportshub.cbsistatic.com/i/r/2024/02/20/2a164257-5fdb-4a4d-b35d-ddd2910b9a03/thumbnail/1200x675/9ef23fe138bb16c9508da6f74f841a5c/cousins-g.jpg"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <a href="/newsdetail" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    );
   }
 }
