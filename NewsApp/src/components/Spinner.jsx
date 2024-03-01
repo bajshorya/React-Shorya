80cc58c39e216515a1717be0d08bb34e69e2f61d
SHORYA BAJ <jain.shorya11@gmail.com>
2024-03-01 21:36:21 +0530

    refactored components to function based component

--- NewsApp/src/components/Spinner.jsx
+++ NewsApp/src/components/Spinner.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
 import SpinnerImg from "./Spinner.gif";
 
-export class Spinner extends Component {
-  render() {
-    return (
-      <div className="text-center">
-        <img src={SpinnerImg} alt="loading" />
-      </div>
-    );
-  }
-}
+const Spinner = () => {
+  return (
+    <div className="text-center">
+      <img src={SpinnerImg} alt="loading" />
+    </div>
+  );
+};
 
 export default Spinner;
