1e1d98839e9cc43aa86c65cf106780ef52776a94
SHORYA BAJ <jain.shorya11@gmail.com>
2024-04-24 11:45:14 +0530

    practise hrkirat cohort

---
+++ practise/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://sum-server.100xdevs.com/todos")
+      .then(function (response) {
+        setTodos(response.data.todos);
+      });
+  }, []);
+
+  return (
+    <>
+      {todos.map((todo) => (
+        <Todo title={todo.title} description={todo.description} />
+      ))}
+    </>
+  );
+}
+function Todo({ title, description }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      {description}
+    </div>
+  );
+}
+
+export default App;
