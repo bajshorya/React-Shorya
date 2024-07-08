a6a2f08d3e74b2c740d494cf01455ae7aac81ba4
SHORYA BAJ <jain.shorya11@gmail.com>
2024-07-08 19:29:22 +0530

    hooks checkpoint

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -2,17 +2,32 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 
-function useTodos() {
+function useTodos(n) {
   const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
+    const value = setInterval(() => {
+      axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
+        setTodos(res.data.todos);
+        setLoading(false);
+      });
+    }, n * 1000);
     axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
       setTodos(res.data.todos);
+      setLoading(false);
     });
-  }, []);
-  return todos;
+    return () => {
+      clearInterval(value);
+    };
+  }, [n]);
+  return { todos, loading };
 }
 function App() {
-  const todos = useTodos();
+  const { todos, loading } = useTodos(5);
+  if (loading) {
+    return <div>loading...</div>;
+  }
   return (
     <>
       <div>
